fafd5921cc897532c80cc5577449e8c5191253d1
a@b.com <a@b.com>
2025-09-25 19:35:26 +0200

    componente principal con estilo global

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import GlobalStyles from "./styles/GlobalStyles";
 function App(){
   return (
-    <View style={styles.contenedor}>
-    <Text style={styles.texto}>Hola</Text>
-    <Text style={styles.texto}>Mundo</Text>
+    <View style={GlobalStyles.contenedor}>
+    <Text style={GlobalStyles.texto}>Hola</Text>
+    <Text style={GlobalStyles.texto}>Mundo</Text>
     </View>
   )
 }
---
+++ styles/GlobalStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+const GlobalStyles = StyleSheet.create({
+    contenedor:{
+        marginTop:300,
+        marginLeft:150
+    },
+    texto:{
+        fontSize:30
+    }
+})
+export default GlobalStyles
