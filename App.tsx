36d127efd70b1adf299627b7ffe36ec8f096a1e8
a@b.com <a@b.com>
2025-09-25 19:25:31 +0200

    finalizado el componente principal

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 export default function App(){
   return (
-    <View>
-    <Text>Hola</Text>
-    <Text>Mundo</Text>
+    <View style={styles.contenedor}>
+    <Text style={styles.texto}>Hola</Text>
+    <Text style={styles.texto}>Mundo</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  contenedor:{
+    marginTop: 300,
+    marginLeft: 80
+  },
+  texto: {
+    fontSize: 24
+  }
+})
