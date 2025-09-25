75efef1ea8ccf87f2eb78bc9377cb7d7738b1eb6
a@b.com <a@b.com>
2025-09-25 19:42:17 +0200

    componente principal en forma de clase y estilo global

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import GlobalStyles from "./styles/GlobalStyles";
-const App = () => {
-  return (
-    <View style={GlobalStyles.contenedor}>
-    <Text style={GlobalStyles.texto}>Hola</Text>
-    <Text style={GlobalStyles.texto}>Mundo</Text>
-    </View>
-  )
+class App extends Component {
+  render (){
+    return (
+      <View style={GlobalStyles.contenedor}>
+        <Text style = {GlobalStyles.texto}>Hola</Text>
+        <Text style = {GlobalStyles.texto}>Mundo</Text>
+      </View>
+    )
+  }
 }
 export default App
